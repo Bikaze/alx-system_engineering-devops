62d46edeceba1b504e62b95ea8f5379e4b344eda
Bikaze <clmntmugisha@gmail.com>
2024-05-07 23:51:20 +0200

    Created 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This is a function that gets nbr of subscribers on a given Subreddit"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    results = response.json().get("data")
+    if not results:
+        return 0
+    return results.get("subscribers")
